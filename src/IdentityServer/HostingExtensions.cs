74ad24ea85e8de82e21774608e30b062c50c30b2
Adam Howell <adam.howell@tombola.com>
2023-02-21 09:22:42 +0000

    Add project files.

---
+++ src/IdentityServer/HostingExtensions.cs
@@ -0,0 +1,43 @@
+using Duende.IdentityServer.Test;
+using Serilog;
+
+namespace IdentityServer;
+
+internal static class HostingExtensions
+{
+    public static WebApplication ConfigureServices(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddIdentityServer()
+            .AddInMemoryIdentityResources(Config.IdentityResources)
+            .AddInMemoryApiScopes(Config.ApiScopes)
+            .AddInMemoryClients(Config.Clients)
+            .AddTestUsers(TestUsers.Users);
+
+        builder.Services.AddAuthorization();
+        builder.Services.AddRazorPages();
+
+        return builder.Build();
+    }
+        
+    public static WebApplication ConfigurePipeline(this WebApplication app)
+    { 
+        app.UseSerilogRequestLogging();
+    
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseDeveloperExceptionPage();
+        }
+
+        // uncomment if you want to add a UI
+        app.UseStaticFiles();
+        app.UseRouting();
+
+        app.UseIdentityServer();
+
+        // uncomment if you want to add a UI
+        app.UseAuthorization();
+        app.MapRazorPages().RequireAuthorization();
+
+        return app;
+    }
+}
